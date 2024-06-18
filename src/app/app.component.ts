774dd55b22bc1f58d91cb3ef56b8a478b9991a60
davidkov111 <kovacsdavid648@outlook.com>
2024-06-18 22:42:06 +0300

    themeing with switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, Inject, OnInit, PLATFORM_ID, Renderer2} from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule, MatToolbarModule],
+  imports: [
+    RouterModule, 
+    RouterOutlet,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  title='UI'
+  isDarkTheme = true;
+
+  constructor(private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      this.isDarkTheme = prefersDark;
+      this.updateTheme();
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.updateTheme();
+  }
+
+  private updateTheme() {
+    if (this.isDarkTheme) {
+      this.renderer.addClass(document.body, 'dark-theme');
+      this.renderer.removeClass(document.body, 'light-theme');
+    } else {
+      this.renderer.addClass(document.body, 'light-theme');
+      this.renderer.removeClass(document.body, 'dark-theme');
+    }
+  }
+}
